2a42f3bf6c671e93b5e69cc7eb55dcf504158f50
B-Nilson <nilson.brayden@gmail.com>
2026-03-13 14:07:12 -0700

    abstract make_aqsu_status_map()

--- R/aqsu_flagged_sites_map.R
+++ R/aqsu_flagged_sites_map.R
@@ -1,182 +1,197 @@
-# Packages/Functions ------------------------------------------------------
-
-source("R/load_report_data.R")
-source("R/format_data.R")
-source("R/flagging.R")
-source("R/map_controls.R")
-source("R/monitor_markers.R")
-source("R/marker_hovers.R")
-source("R/marker_popups.R")
-source("R/plotting.R")
-
-# Import fonts and functions
-# extrafont::font_import() # only run once per machine
-# extrafont::loadfonts(device = "all", quiet = TRUE)
-
-# Setup -------------------------------------------------------------------
-
 # What data to display
-latest_date <- lubridate::now("UTC")
-duration_days <- 7
-averaging_period <- "10 mins"
-desired_cols <- c(
-  "site_id",
-  "date",
-  pm25_qc = "qaqc_flag_pm2.5",
+meta_cols <- c("site_id", "date", pm25_qc = "qaqc_flag_pm2.5")
+value_cols <- c(
   pm25 = "pm2.5_validated",
   pm25_a = "pm2.5_a",
   pm25_b = "pm2.5_b",
   "temperature",
   "rh"
 )
-value_cols <- desired_cols[-(1:3)]
-value_cols <- ifelse(names(value_cols) == "", value_cols, names(value_cols))
+timestamp_tz <- "America/Edmonton"
 
-# How to display data
-popup_width_px <- 600
-page_title <- "AQSU Status"
-p_hours_flagged_thresh <- 0.15
-
-# Where to save things
-report_dir <- "deployments"
-img_dir <- "plots" # relative to report_dir
-obs_cache_rds <- "data/aqsu_past_2_week.rds"
-output_paths <- list(
-  map = "deployments/index.html",
-  data = "deployments/aqsu_monitor_status.csv"
-)
-
-# What JS/CSS files to include?
-js_css_paths <- c("css/report_stylesheet.css", "js/main.js", "js/helpers.js")
-
-# TODO: include as default in aqmapr
-inter_font_url <- c(
-  "Inter" = 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
-)
-
-earliest_date <- latest_date - lubridate::days(duration_days)
-
-# Load Past 2 Weeks of AQSU Data ------------------------------------------
-
-dirname(obs_cache_rds) |> dir.create(showWarnings = FALSE, recursive = TRUE)
-obs <- latest_date |>
-  load_report_data(
-    duration_days = duration_days,
-    desired_cols = desired_cols,
-    cache_path = obs_cache_rds
-  )
-
-# Flag Data ---------------------------------------------------------------
-
-# Add "_flagged" names for passing to dplyr::across()
-value_cols_flagged <- value_cols |>
-  stats::setNames(paste0(value_cols, "_flagged"))
-
-obs <- obs |>
-  # TODO: aggregate to averaging period if needed
-  # Fill in from start of data record for a specific site to last date of data from any site
-  complete_active_site_records(
-    time_step = averaging_period,
-    duration_days = duration_days
-  ) |>
-  # Flag temperature/rh data and breakdown pm25 flag
-  add_purpleair_flags() |>
-  apply_purpleair_flags(value_cols_flagged = value_cols_flagged) |>
-  dplyr::arrange(site_id, date)
-
-# Make summary dataset ----------------------------------------------------
-
-# Summarise flagging for each monitor
-map_data <- obs |>
-  make_status_map_summary(
-    value_cols = value_cols_flagged,
-    flag_threshold = p_hours_flagged_thresh
-  )
-
-# Make timeseries data for each monitor
-plot_cols <- c(
-  "site_id",
-  "name",
-  "date",
+make_aqsu_status_map <- function(
+  latest_date = lubridate::now("UTC"),
+  duration_days = 7,
+  averaging_period = "10 mins",
+  p_hours_flagged_thresh = 0.15,
+  meta_cols,
   value_cols,
-  paste0(value_cols, "_flag")
-)
-map_data$plot_data <- obs |>
-  dplyr::select(dplyr::all_of(plot_cols)) |>
-  dplyr::mutate(site_id_copy = site_id) |>
-  tidyr::nest(.by = site_id_copy) |>
-  dplyr::pull(data)
+  obs_cache_rds = "data/aqsu_past_%s_days.rds" |> sprintf(duration_days),
+  timestamp_tz = "browser",
+  base_map_provider = c("Light Theme" = "OpenStreetMap"),
+  report_dir = "deployments",
+  img_dir = "plots",
+  map_lib_dir = "libs",
+  css_dir = "css",
+  js_dir = "js",
+  map_path = "%s/index.html" |> sprintf(report_dir),
+  data_path = "%s/aqsu_monitor_status.csv" |> sprintf(report_dir),
+  page_title = "AQSU Status",
+  popup_width_px = 600,
+  save_figures = TRUE # set to FALSE for faster testing
+) {
+  # Import fonts and functions
+  # extrafont::font_import() # only run once per machine
+  # extrafont::loadfonts(device = "all", quiet = TRUE)
 
-# Make popup html, creating and saving figures along the way
-file.path(report_dir, img_dir) |>
-  dir.create(showWarnings = FALSE, recursive = TRUE)
-map_data <- map_data |>
-  dplyr::mutate(
-    popup = .data$plot_data |>
-      handyr::for_each(
-        .enumerate = TRUE,
-        \(pd, i) {
-          pd |>
-            aqsu_status_popup(
-              earliest_date = earliest_date,
-              report_path = report_dir,
-              img_dir = img_dir,
-              width = popup_width_px,
-              is_missing = map_data$entirely_offline[i],
-              save_figures = TRUE # set to FALSE for faster testing
-            )
-        }
+  source("R/load_report_data.R")
+  source("R/format_data.R")
+  source("R/flagging.R")
+  source("R/map_controls.R")
+  source("R/monitor_markers.R")
+  source("R/marker_hovers.R")
+  source("R/marker_popups.R")
+  source("R/plotting.R")
+
+  js_css_paths <- file.path(css_dir, "report_stylesheet.css") |>
+    c(file.path(js_dir, c("main.js", "helpers.js")))
+  inter_font_url <- '%s?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap' |>
+    sprintf("https://fonts.googleapis.com/css2") |>
+    setNames("Inter")
+  earliest_date <- latest_date - lubridate::days(duration_days)
+  desired_cols <- c(meta_cols, value_cols)
+  value_cols <- ifelse(names(value_cols) == "", value_cols, names(value_cols))
+
+  value_cols_flagged <- value_cols |>
+    stats::setNames(paste0(value_cols, "_flagged"))
+
+  # Load Past 2 Weeks of AQSU Data and flag
+  dirname(obs_cache_rds) |> dir.create(showWarnings = FALSE, recursive = TRUE)
+  obs <- latest_date |>
+    load_report_data(
+      duration_days = duration_days,
+      desired_cols = desired_cols,
+      cache_path = obs_cache_rds
+    ) |>
+    # TODO: aggregate to averaging period if needed
+    # Fill in from start of data record for a specific site to last date of data from any site
+    complete_active_site_records(
+      time_step = averaging_period,
+      duration_days = duration_days
+    ) |>
+    # Flag temperature/rh data and breakdown pm25 flag
+    add_purpleair_flags() |>
+    apply_purpleair_flags(value_cols_flagged = value_cols_flagged) |>
+    dplyr::arrange(site_id, date)
+
+  # Summarise flagging for each monitor
+  map_data <- obs |>
+    make_status_map_summary(
+      value_cols = value_cols_flagged,
+      flag_threshold = p_hours_flagged_thresh
+    )
+
+  # Make timeseries data for each monitor
+  plot_cols <- c(
+    "site_id",
+    "name",
+    "date",
+    value_cols,
+    paste0(value_cols, "_flag")
+  )
+  map_data$plot_data <- obs |>
+    dplyr::select(dplyr::all_of(plot_cols)) |>
+    dplyr::mutate(site_id_copy = site_id) |>
+    tidyr::nest(.by = site_id_copy) |>
+    dplyr::pull(data)
+
+  # Make popup html, creating and saving figures along the way
+  file.path(report_dir, img_dir) |>
+    dir.create(showWarnings = FALSE, recursive = TRUE)
+  map_data <- map_data |>
+    dplyr::mutate(
+      popup = .data$plot_data |>
+        handyr::for_each(
+          .enumerate = TRUE,
+          \(pd, i) {
+            pd |>
+              aqsu_status_popup(
+                earliest_date = earliest_date,
+                report_path = report_dir,
+                img_dir = img_dir,
+                width = popup_width_px,
+                is_missing = map_data$entirely_offline[i],
+                save_figures = save_figures
+              )
+          }
+        )
+    )
+
+  # Make Map of AQSU Flags
+  layer_groups <- c(
+    "PM2.5 Sensors",
+    "Temperature Sensor",
+    "Humidity Sensor",
+    "Current PM2.5",
+    "Current Temperature",
+    "Current RH"
+  )
+  if (is.null(names(base_map_provider))) {
+    names(base_map_provider) <- base_map_provider # TODO: do in aqmapr
+  }
+  map <- aqmapr::make_leaflet_map(
+    base_maps = base_map_provider,
+    layer_control_titles = NULL,
+    add_basemaps_to_layer_control = FALSE,
+    page_title = page_title,
+    track_map_state = FALSE,
+    center_on_opened_popup = TRUE,
+    include_scalebar = FALSE
+  ) |>
+    leaflet.extras::addHash() |> # track map center/zoom
+    aqmapr::include_font(font_urls = inter_font_url, force = TRUE) |>
+    aqmapr::add_map_timestamp(
+      timestamp = max(obs$date) |>
+        lubridate::with_tz(ifelse(
+          timestamp_tz == "browser",
+          yes = "UTC",
+          no = timestamp_tz
+        )),
+      use_browser_timezone = timestamp_tz == "browser"
+    ) |>
+    # Add layers control to topright
+    leaflet::addLayersControl(
+      overlayGroups = layer_groups,
+      options = leaflet::layersControlOptions(collapsed = FALSE)
+    ) |>
+    add_monitor_markers(
+      map_data = map_data,
+      duration_days = duration_days,
+      popup_width_px = popup_width_px
+    ) |>
+    # Add search menu and scale bar
+    add_search_menu(
+      target_groups = layer_groups[1],
+      search_property = "label"
+    ) |>
+    leaflet::addScaleBar(position = "bottomleft") |>
+    # Include custom JS/CSS
+    aqmapr::include_scripts(paths = js_css_paths) |>
+    htmlwidgets::onRender("handle_render")
+
+  # Save map to html if desired
+  if (!is.null(map_path)) {
+    map |>
+      aqmapr::save_map(
+        save_to = map_path,
+        library_dir = map_lib_dir
       )
-  )
+  }
 
-# Map of AQSU Flags -------------------------------------------------------
+  # Save data if desired
+  if (!is.null(data_path)) {
+    map_data |>
+      dplyr::select(-plot_data, -popup, -dplyr::starts_with("hover_")) |>
+      data.table::fwrite(data_path)
+  }
 
-aqmapr::make_leaflet_map(
-  base_maps = c(`Light Theme` = "OpenStreetMap"),
-  layer_control_titles = NULL,
-  add_basemaps_to_layer_control = FALSE,
-  page_title = page_title,
-  track_map_state = FALSE,
-  center_on_opened_popup = TRUE,
-  include_scalebar = FALSE
-) |>
-  leaflet.extras::addHash() |> # track map center/zoom
-  aqmapr::include_font(font_urls = inter_font_url, force = TRUE) |>
-  aqmapr::add_map_timestamp(
-    timestamp = max(obs$date) |> lubridate::with_tz("America/Edmonton"),
-    use_browser_timezone = FALSE
-  ) |>
-  # Add layers control to topright
-  leaflet::addLayersControl(
-    overlayGroups = c(
-      "PM2.5 Sensors",
-      "Temperature Sensor",
-      "Humidity Sensor",
-      "Current PM2.5",
-      "Current Temperature",
-      "Current RH"
-    ),
-    options = leaflet::layersControlOptions(collapsed = FALSE)
-  ) |>
-  add_monitor_markers(
-    map_data = map_data,
-    duration_days = duration_days,
-    popup_width_px = popup_width_px
-  ) |>
-  # Add search menu and scale bar
-  add_search_menu(target_groups = "PM2.5 Sensors", search_property = "label") |>
-  leaflet::addScaleBar(position = "bottomleft") |>
-  # Include custom JS/CSS
-  aqmapr::include_scripts(paths = js_css_paths) |>
-  htmlwidgets::onRender("handle_render") |>
-  # Save map to html page
-  aqmapr::save_map(
-    save_to = output_paths$map,
-    library_dir = "libs"
-  )
+  invisible(map)
+}
 
-map_data |>
-  dplyr::select(-plot_data, -popup, -dplyr::starts_with("hover_")) |>
-  data.table::fwrite(output_paths$data)
+make_aqsu_status_map(
+  meta_cols = meta_cols,
+  value_cols = value_cols,
+  timestamp_tz = timestamp_tz
+)
 
 # TODO: priority list for repair/replace with info on install/failure date, what failed, env. conditions ecposed to
