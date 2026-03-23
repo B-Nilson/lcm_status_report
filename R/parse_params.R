f3acc9ce1fa9d652d4febee96833475db92edc64
Brayden Nilson <nilson.brayden@gmail.com>
2026-03-23 11:35:40 -0700

    fix typo dplyr:: -> lubridate::

--- R/parse_params.R
+++ R/parse_params.R
@@ -2,7 +2,7 @@ parse_params <- function(params_file = "params.yml") {
   params <- yaml::read_yaml(params_file)
   if (params$report_period$latest_date == "now") {
     params$report_period$latest_date <- lubridate::now("UTC") |> 
-      dplyr::floor_date(params$report_period$averaging_period)
+      lubridate::floor_date(params$report_period$averaging_period)
   } else {
     params$report_period$latest_date <- params$report_period$latest_date |>
       lubridate::ymd_hms(tz = "UTC")
